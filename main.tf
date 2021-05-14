aa3b37657374952e8d63d5ae99bc460584699b0d
William Marques <william.marques@zoop.co>
2021-05-14 14:41:53 -0300

    Apply Zoop tag definitions

--- main.tf
+++ main.tf
@@ -1,22 +1,28 @@
 locals {
-  id             = lower(join(var.delimiter, compact(concat(list(var.application, var.namespace, var.environment), var.attributes))))
-  application    = lower(format("%v", var.application))
-  service        = lower(format("%v", var.service))
-  environment    = lower(format("%v", var.environment))
-  business_owner = lower(format("%v", var.business_owner))
-  domain_name    = var.environment == "production" ? lower(var.application) : lower("${var.application}.${var.environment}")
-  namespace      = lower(format("%v", var.namespace))
-  delimiter      = lower(format("%v", var.delimiter))
-  attributes     = lower(format("%v", join(var.delimiter, compact(var.attributes))))
+  id                 = lower(join(var.delimiter, compact(concat(compact(tolist(["zp",""])), list(var.environment, var.namespace, var.application), var.attributes))))
+  application        = lower(format("%v", var.application))
+  product            = lower(format("%v", var.product))
+  environment        = lower(format("%v", var.environment))
+  business_owner     = lower(format("%v", var.business_owner))
+  domain_name        = var.environment == "production" ? lower(var.application) : lower("${var.application}.${var.environment}")
+  namespace          = lower(format("%v", var.namespace))
+  delimiter          = lower(format("%v", var.delimiter))
+  attributes         = lower(format("%v", join(var.delimiter, compact(var.attributes))))
+  organization_owner = lower(format("%v", var.organization_owner))
+  backup             = lower(format("%v", var.backup_status))
+  monitoring         = lower(format("%v", var.monitoring_status))
 
   tags = merge(
     {
-      "Name"           = local.id
-      "application"    = local.application
-      "service"        = local.service
-      "environment"    = local.environment
-      "business_owner" = local.business_owner
-      "terraform"      = "true"
+      "Name"               = local.id
+      "product"            = local.product
+      "application"        = local.application
+      "organization-owner" = local.organization_owner
+      "business-owner"     = local.business_owner
+      "environment"        = local.environment
+      "backup"             = local.backup
+      "monitoring"         = local.monitoring
+      "terraform"          = true
     },
     var.tags
   )
